b4a2808a0a91c553ffaaed56bac5fe7f48a0a2a2
Luis Soto <luis.a.soto.b@gmail.com>
2017-05-22 20:15:27 -0400

    Modifica clases formulario y reemplaza prescripcion por detalle formulario (de acuerdo a las base de datos) LS

--- Negocio/Formulario.cs
+++ Negocio/Formulario.cs
@@ -8,13 +8,12 @@ namespace Negocio
 {
     public class Formulario
     {
-        public int Codigo { get; set; }
-        public DateTime FechaIngreso { get; set; }
-        public Medico medico { get; set; }
-        public Paciente paciente { get; set; }
-        public Medicamento medicam { get; set; }
-        public Prescripcion presc { get; set; }
-        public bool Control { get; set; }
+        public int IdForm { get; set; }
+        public DateTime FechaEmison { get; set; }
+        public string TipoTratamiento { get; set; }
+        public string RutPaciente { get; set; }
+        public string MedicoRutUser { get; set; }
+       
 
         public Formulario()
         {
@@ -23,13 +22,12 @@ namespace Negocio
 
         private void Init()
         {
-            this.Codigo = 0;
-            this.FechaIngreso = DateTime.Today;
-            this.medico = medico;
-            this.paciente = paciente;
-            this.medicam = medicam;
-            this.presc = presc;
-            this.Control = false;
+            this.IdForm = 0;
+            this.FechaEmison = DateTime.Today;
+            this.TipoTratamiento = string.Empty;
+            this.RutPaciente = string.Empty;
+            this.MedicoRutUser = string.Empty;
+            
         }
     }
 }
